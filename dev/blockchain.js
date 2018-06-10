3f322b7b5827a0bb6ba32af893efe7eb93bd5bac
Wellinton Monge <wellmonge@users.noreply.github.com>
2018-06-10 10:06:11 -0300

    creating data structure from my blockchain

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,16 +1,20 @@
-function Blockchain(params) {
+function Blockchain() {
     this.chain = [];
-    this.newTransaction = [];
+    this.newTransactions = [];
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transaction: this.newTransaction,
+        transaction: this.newTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash,
     };
 
+    this.newTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
 }
