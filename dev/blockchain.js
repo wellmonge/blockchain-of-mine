e0f5ded3fda6c1873849170a0f4051cac6d6972b
Wellinton Monge <wellmonge@users.noreply.github.com>
2018-07-18 21:55:46 -0300

    new Transaction

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,21 +1,36 @@
 function Blockchain() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transaction: this.newTransactions,
+        transaction: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash,
     };
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
     
     return newBlock;
+}
+Blockchain.prototype.getLastBlock = function(){
+    return this.chain[this.chain.length - 1];
+}
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
+    const newTransactions = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient   
+    }
+
+    this.pendingTransactions.push(newTransactions);
+
+    return this.getLastBlock()['index'] + 1;
+
 }
 
 module.exports = Blockchain;
--- test/index.js
+++ test/index.js
@@ -1,1 +1,21 @@
-const Blockchain = require('');
+const Blockchain = require('../dev/blockchain');
+
+const mongecoin = new Blockchain(); 
+
+mongecoin.createNewBlock(123456,'SAIHJDASOJIDS','JDJAIODSJAODJSA');
+
+mongecoin.createNewTransaction(100,'MONGESADASFDSADF','ALBERTPTKROPYHRTK');
+
+mongecoin.createNewBlock(213123,'VEVREFDVSADSFSFS','JDJAIODSJAODJSA');
+
+
+mongecoin.createNewTransaction(50,'MONGESADASFDSADF','ALBERTPTKROPYHRTK');
+
+mongecoin.createNewTransaction(300,'MONGESADASFDSADF','ALBERTPTKROPYHRTK');
+
+mongecoin.createNewTransaction(2000,'MONGESADASFDSADF','ALBERTPTKROPYHRTK');
+
+mongecoin.createNewBlock(2131232,'DASFASWFGSDAGFSD','ÇLOIÇLIOÇIOÇOI');
+
+
+console.log(mongecoin.chain[2]);
