9585fb4226d7423053b8d4e55b937db8d38d70a2
wellinton monge <mongewellinton@gmail.com>
2018-07-21 09:21:14 -0300

    ...api test bcom

--- api.js
+++ api.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./dev/blockchain');
+const uuid = require('uuid/v1');
 
+const nodeAddress = uuid().split('-').join('');
+console.log(nodeAddress);
 const mongecoin = new Blockchain();
 
 app.use(bodyParser.json());
@@ -13,12 +16,34 @@ app.get('/blockchain',function(req,res){
 });
 
 
-app.get('/transaction',function(req,res){
-    res.send('teste');
+app.post('/transaction',function(req,res){
+    const blockIndex = mongecoin.createNewTransaction(req.body.amount,req.body.sender,req.body.recipient);
+
+    res.json({
+        note: `Transaction will be add in block ${blockIndex}.`
+    });
 });
 
 app.get('/mine',function(req,res){
-    res.send('teste');
+    const lastBlock = mongecoin.getLastBlock();
+    const previousBlockHash =  lastBlock['hash'];
+    const currentBlockData = {
+        transactions:mongecoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    }
+
+    const nonce = mongecoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = mongecoin.hashBlock(previousBlockHash, currentBlockData,nonce);
+
+    mongecoin.createNewTransaction(12.5,`00`,nodeAddress);
+
+    const newBlock = mongecoin.createNewBlock(nonce, previousBlockHash, blockHash);
+    res.json({
+        note:'New block mined successfully',
+        block: newBlock
+    });  
 });
 
-app.listen(3000);
+app.listen(3000, function () {
+console.log('up and running');    
+});
--- dev/blockchain.js
+++ dev/blockchain.js
@@ -49,10 +49,9 @@ Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, n
 Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
     let nonce = 0;
     let hash = this.hashBlock(previousBlockHash,currentBlockData, nonce);
-    while (hash.substring(0, 4) !== 'm4g5'){
+    while (hash.substring(0, 4) !== '0000'){
         nonce++;
         hash = this.hashBlock(previousBlockHash,currentBlockData, nonce);
-        console.log(hash)
     }
     return nonce;
 }
